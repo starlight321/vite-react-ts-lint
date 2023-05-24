e90a97681676f70c03dd79eb71e1e25d4216f353
mingxindong <xindong.ming@avatr.com>
2023-05-24 17:33:46 +0800

    feat: stylelint 校验

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
         'function-name-case': 'lower',
         'no-descending-specificity': null,
         'no-invalid-double-slash-comments': null,
-        'rule-empty-line-before': 'always',
+        'rule-empty-line-before': 'never',
       }
     }
   ],
@@ -22,16 +22,7 @@ module.exports = {
   rules: {
     "block-no-empty": null,  // 不检测 禁止出现空块 规则
     "color-no-invalid-hex": true, // 禁止使用无效的十六进制颜色
-    "max-empty-lines": 2,
-    'function-url-quotes': 'always',
-    'string-quotes': 'double', 
     'indentation': 2,
-    'unit-case':null,
-    'color-hex-case': 'upper',
-    'color-hex-length': 'long',
-    'font-family-no-missing-generic-family-keyword': null,
     'block-opening-brace-space-before':'always',
-    'property-no-unknown':null,
-    'no-empty-source':null,
   }
 };
